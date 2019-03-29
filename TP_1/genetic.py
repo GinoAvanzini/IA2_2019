fb47a79c4a80aa2263eecc1290f04199e7f50241
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2019-03-29 18:01:19 -0300

    [GA] first version

--- TP_1/genetic.py
+++ TP_1/genetic.py
@@ -1,20 +1,21 @@
-from random import randint
+from random import randint, choices
 from numpy.random import choice
 
 from simulated_annealing import temple_simulado, map_to_coord, neighbours_annealing, distance
 
-N_POB = 20
+N_POB = 10
 MAX_LENGHT = 31
-T_0 = 300
+T_0 = 100
+MAX_GEN = 10
+
+MUT_PROB = 30
 
 
 # [INDEPENDIENTE DEL PROBLEMA]
-def crossover(ind1, ind2):
+def cross_points(dim):
     """
-    Función de crossover entre individuos. Recibe las lista del estado de cada indiviuo. Crossover por *cruce de orden*.
+    Función interna de la implementación del algoritmo genético. Recibe la dimensión del individuo (cantidad de componentes) y devuelve dos índices aleatorios ordenados de menor a mayor.
     """
-
-    dim = len(ind1)
 
     c1 = randint(1, dim-2)
     c2 = randint(1, dim-2)
@@ -27,6 +28,20 @@ def crossover(ind1, ind2):
         c1 = c2
         c2 = aux
 
+    return c1, c2
+
+
+# [INDEPENDIENTE DEL PROBLEMA]
+def crossover(ind1, ind2):
+    """
+    Función de crossover entre individuos. Recibe las lista del estado de cada indiviuo. Crossover por *cruce de orden*.
+    """
+
+    dim = len(ind1)
+
+    c1, c2 = cross_points(dim)
+
+    # Construcción del individuo
     ans1 = [-1] * c1 + ind2[c1:c2+1] + [-1] * (dim - c2 - 1)
     ans2 = [-1] * c1 + ind1[c1:c2+1] + [-1] * (dim - c2 - 1)
 
@@ -53,16 +68,7 @@ def mutacion(ind):
 
     dim = len(ind)
 
-    c1 = randint(0, dim-1)
-    c2 = randint(0, dim-1)
-
-    while (c2 == c1): # Verificación para que sean distintos los puntos de cruce
-        c2 = randint(0, dim-1)
-
-    if (c2 < c1): # Ordenar los puntos de cruce de menor a mayor
-        aux = c1
-        c1 = c2
-        c2 = aux
+    c1, c2 = cross_points(dim)
 
     aux = ind[c2]
     ind.remove(ind[c2])
@@ -71,9 +77,66 @@ def mutacion(ind):
     return ind
 
 
-def genetic(init):
-    pass
+# [INDEPENDIENTE DEL PROBLEMA]
+def genetic(pob):
 
+    count = 0
+    while (count < MAX_GEN):
+
+        # Cálculo de fitness de cada individuo de la población
+        fit = []
+        total_fit = 0
+
+        for ind in pob:
+            value = fitness(ind)
+            fit.append(value)
+            total_fit += value
+
+            weight = []
+
+        for item in fit:
+            weight.append(1 - (item / total_fit))
+
+        new_pob = []
+        while (len(new_pob) < N_POB):
+
+            # Selección de padres por peso de fitness: https://en.wikipedia.org/wiki/Fitness_proportionate_selection
+            [ind1, ind2] = choices(population=pob, k=2, weights=weight)
+
+            s1, s2 = crossover(ind1, ind2)
+
+
+            if (randint(0, 100) < MUT_PROB):
+                s1 = mutacion(s1)
+
+            if (randint(0, 100) < MUT_PROB):
+                s2 = mutacion(s1)
+
+            new_pob.append(s1)
+            new_pob.append(s2)
+
+        pob = new_pob
+
+        count += 1
+
+    # Selección del mejor de la población
+    fit = []
+
+    for ind in pob:
+        value = fitness(ind)
+        fit.append(value)
+
+    max_index = 0
+    for i in range(0, len(pob)):
+        if (fit[i] < fit[max_index]):
+            max_index = i
+
+    return pob[max_index]
+
+
+#-------------------------------------------------------------------------------
+#   MODELADO DEL PROBLEMA
+#-------------------------------------------------------------------------------
 
 # [DEPENDIENTE DEL PROBLEMA]
 def fitness(ind):
@@ -82,16 +145,21 @@ def fitness(ind):
     Depende del problema, en este caso lo da el algoritmo de recocido simulado.
     """
     path, cost = temple_simulado(map_to_coord(ind), T_0, neighbours_annealing, distance)
-    return cost/len(ind)
+    return cost
 
 
+# [DEPENDIENTE DEL PROBLEMA]
 def generate_ind():
-    individuo = {}
+    """
+    Función para generar un individuo de forma aleatoria para incorporar a la población inicial. Devuelve el individuo, en este caso, una lista de números que corresponden a la ubicación de cada producto.
+    """
+
+    individuo = []
     aux = list(range(0, MAX_LENGHT))
     for i in range(0, MAX_LENGHT):
         item = choice(aux)
         aux.remove(item)
-        individuo[i] = item
+        individuo.append(item)
 
     return individuo
 
@@ -100,14 +168,6 @@ if __name__ == "__main__":
     start = []
     for i in range(0, N_POB):
         start.append(generate_ind())
-        # print(start[i])
 
-    print(start[0].values())
-    print(start[1].values())
-    ans1, ans2 = crossover(list(start[0].values()), list(start[1].values()))
-    print(ans1, fitness(list(ans1)))
-    print(ans2, fitness(list(ans2)))
-
-    print("org ", ans1)
-    ans1 = mutacion(ans1)
-    print("mut ", ans1)
+    best = genetic(start)
+    print(best)
