e1ef3f642cc9568a489b0ab1a8cc06edad28a36d
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2019-05-02 01:59:52 -0300

    [BS] added jupyter notebook

--- TP_1/backtracking.py
+++ TP_1/backtracking.py
@@ -1,14 +1,11 @@
 from random import choice
 
-N = 5 # Número de tareas
 DLINE = 150 # Tiempo en que se debe finalizar el proceso en minutos
 STEP = 5 # Discretización del tiempo
 
-T = {"d0": 40, "d1":5, "d2":15, "d3":10, "d4":30}
-
 def selection(D, assign):
     """
-    Selección de variable con heurística *variable más restringida*. La función recibe el diccionario de dominio de las distintas variables "D" y un diccionario con las variables ya asignadas "assign".
+    Selección de variable no asignada de forma aleatoria. La función recibe el diccionario de dominio de las distintas variables "D" y un diccionario con las variables ya asignadas "assign".
     La función devuelve el key de diccionario asociado a la variable elegida.
     """
     k_D = list(D.keys())
@@ -19,7 +16,7 @@ def selection(D, assign):
     return choice(k)
 
 
-def consistent(var, value, assign, R2, M):
+def consistent(var, value, T, assign, R2, M):
     """
     Función para comprobar la consistencia de una posible asignación de valor a una variable. Dentro de esta función se verifica que la asignación respete todas las restricciones posibles con las variables ya asignadas, devolviendo verdadero; o no haya consistencia en la asignación, devolviendo falso.
     Recibe como parámetros la variable a asignar "var", el valor a asignar a dicha variable "value", el diccionario con todas las asignaciones ya realizadas "assign" y un diccionario con las restricciones asociadas a cada variable "R2".
@@ -42,21 +39,21 @@ def consistent(var, value, assign, R2, M):
     return True
 
 
-def backtrack(assign, D, R2, M):
+def backtrack(T, assign, D, R2, M):
     """
     Función principal del algoritmo. Es una búsqueda hacia atrás o backtracking-search, por lo tanto, basicamente es un algoritmo de búsqueda primero en profundidad.
     Recibe como parámetros un diccionario con las asignaciones ya realizadas "assign" (en el primer llamado es un diccionario vacío pero luego se realiza recursión), un diccionario con el dominio de las variables "D" y un diccionario con las restricciones asociadas a las variables "R2".
     Se devuelve la solución del problema si la hay, o un falso si no existe solución.
     """
-    if (len(assign) == N): # Condición de salida
+    if (len(assign) == len(T)): # Condición de salida
         return assign
 
     var = selection(D, assign) # Falta selecciónar solo entre las no asignadas [SOLUCIONADO?]
 
     for i in D[var]:
-        if (consistent(var, i, assign, R2, M)):
+        if (consistent(var, i, T, assign, R2, M)):
             assign[var] = i
-            ans = backtrack(assign, D, R2, M)
+            ans = backtrack(T, assign, D, R2, M)
             if (ans != False):
                 return ans
             assign.pop(var)
@@ -65,6 +62,8 @@ def backtrack(assign, D, R2, M):
 
 if __name__ == "__main__":
 
+    T = {"d0": 40, "d1":5, "d2":15, "d3":10, "d4":30}
+
     D = {}
     for i in T.keys():
         D[i] = list(range(0, DLINE-T[i], STEP))
@@ -72,8 +71,8 @@ if __name__ == "__main__":
     assign = {}
     
     print(selection(D, assign))
-    print(T)
-    print(D)
+    # print(T)
+    # print(D)
 
     # Restricciones de precedencia
     R2 = {
@@ -93,4 +92,4 @@ if __name__ == "__main__":
         "d4":["m0", "m4"]
     }
 
-    print(backtrack(assign, D, R2, M))
+    print(backtrack(T, assign, D, R2, M))
