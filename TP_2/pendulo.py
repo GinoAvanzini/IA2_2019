fecc0f9b24653fef7b049b12690b33b2407beaba
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2019-05-02 12:47:43 -0300

    [FL] profile generator

--- TP_2/pendulo.py
+++ TP_2/pendulo.py
@@ -3,6 +3,20 @@ from math import sin, cos, pow, pi
 from matplotlib import pyplot as plt
 
 
+def generate_profile(center, min, max, step, theta):
+    profile = [0] * len(theta)
+    index_min = theta.index(min)
+    index_center = theta.index(center)
+    index_max = theta.index(max)
+    # profile[index_center] = 1
+    for i in range(index_min, index_center):
+        profile[i] = (i - index_min) / (index_center - index_min)
+    for i in range(index_center, index_max+1):
+        profile[i] = 1 - (i - index_center) / (index_max - index_center)
+
+    return profile
+
+
 def update(x, dt):
 
     x_t = x
@@ -21,45 +35,54 @@ def update(x, dt):
 
 if __name__ == "__main__":
 
+    STEP = 5
+
     #--------------------------------------------------
     # Constantes del modelo
     #--------------------------------------------------
     g = 9.81    # [m/s^2]   Aceleración de la gravedad
-    F = 0      # [N]       Fuerza externa
+    F = 0       # [N]       Fuerza externa
     m = 0.2     # [Kg]      Masa del péndulo
     l = 0.5     # [m]       Longitud del péndulo
     M = 2       # [Kg]      Masa del carro
     #--------------------------------------------------
 
-    dt = 0.001
-    t = 0
+    # dt = 0.001
+    # t = 0
 
-    x = [-pi + 0.01, 0, 0]
+    # x = [-pi + 0.01, 0, 0]
 
-    pos = []
-    vel = []
-    acel = []
-    time = []
+    # pos = []
+    # vel = []
+    # acel = []
+    # time = []
 
-    while(t < 2000):
+    # while(t < 2000):
 
-        pos.append(x[0])
-        vel.append(x[1])
-        acel.append(x[2])
+    #     pos.append(x[0])
+    #     vel.append(x[1])
+    #     acel.append(x[2])
         
-        x = update(x, dt)
+    #     x = update(x, dt)
         
-        time.append(t)
+    #     time.append(t)
 
-        t += dt
+    #     t += dt
 
 
-    fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(16, 5))
-    ax0.plot(time, pos)
-    ax0.grid()
-    ax1.plot(time, vel)
-    ax1.grid()
-    ax2.plot(time, acel)
-    ax2.grid()
+    # fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(16, 5))
+    # ax0.plot(time, pos)
+    # ax0.grid()
+    # ax1.plot(time, vel)
+    # ax1.grid()
+    # ax2.plot(time, acel)
+    # ax2.grid()
     
+    # plt.show()
+
+    theta = list(range(-90, 90+STEP, STEP))
+
+    p1 = generate_profile(30, -10, 75, STEP, theta)
+    plt.plot(theta, p1)
+    plt.grid()
     plt.show()
