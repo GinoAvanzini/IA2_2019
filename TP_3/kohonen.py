d3e6ec34685b5b6300edaec3ead7a762320e8600
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2019-06-13 10:51:12 -0300

    [KM] update kohonen

--- TP_3/kohonen.py
+++ TP_3/kohonen.py
@@ -1,12 +1,12 @@
-
 import csv
 import numpy as np
 from math import inf
 
+from matplotlib import pyplot as plt
+
 from progressBar import printProgressBar
 
-
-def ritmo_aprendizaje(t, t_alfa=100, alfa_0=0.7, alfa_f=0.01):
+def ritmo_aprendizaje(t, t_alfa=20000, alfa_0=0.7, alfa_f=0.01):
     """Obtener el coeficiente de ritmo de aprendizaje dada la iteración actual
 
     Parameters
@@ -27,11 +27,14 @@ def ritmo_aprendizaje(t, t_alfa=100, alfa_0=0.7, alfa_f=0.01):
         El ritmo de aprendizaje para la iteración actual t
     """
 
+    if (t > t_alfa):
+        return alfa_f
+
     alfa = alfa_0 * (alfa_f / alfa_0)**(t / t_alfa)
     return alfa
 
 
-def radio_vecindad(t, t_r=100, R_0=100, R_f=1):
+def radio_vecindad(t, t_r=20000, R_0=3, R_f=1):
     """Obtener el radio de vecindad dada la iteración actual
 
     Parameters
@@ -50,6 +53,9 @@ def radio_vecindad(t, t_r=100, R_0=100, R_f=1):
     float
         El radio de vecindad para la iteración actual t
     """
+
+    if (t > t_r):
+        return R_f
 
     R = R_0 + (R_f - R_0) * t / t_r
     return R
@@ -152,16 +158,28 @@ def update_w(w, x, i, g, t, lvq=1, ritmo_aprendizaje=ritmo_aprendizaje, vecindad
     return w_new
 
 
+def winner(W, x, similarity=d_euclidean):
 
-if __name__ == "__main__":
-    
+    minim = inf
+    for i in range(W.shape[0]):
+        for j in range(W.shape[1]):
+            aux = similarity(x, W[i, j, :])
+            if (aux < minim):
+                minim = aux
+                g = [i, j]
+    return g
+
+
+def get_data(data_file):
     data_raw = []
 
-    with open('train.csv') as csvDataFile:
+    with open(data_file) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
         for i, row in enumerate(csvReader):
             if (i == 0):
                 continue
+            # if (i > 100):
+            #     break
             label = row[0]
             data_raw.append([label, np.array(row[1:], dtype=float)])
 
@@ -169,59 +187,204 @@ if __name__ == "__main__":
     data = []
 
     for i in range(0, len(data_raw)-1):
-        # label = img[0]
         label = data_raw[i+1][0]
+        # label = data_raw[i][0]
         np.delete(data_raw[i][0], 0)
         img_as_float = data_raw[i][1] / 255
         data.append([label, img_as_float])
 
-    # MODELO DE LA RED NEURONAL
-    x = data_raw[0][1]
-    map_kohonen = np.zeros((100, 100)) # Mapa de neuronas
-    W = np.zeros((map_kohonen.shape[0], map_kohonen.shape[1], x.shape[0]))  
-    
-    # k-fold cross validation
-    train = data[:28000]
-    test = data[28000:]
+    return data
 
-    print(len(data), len(train), len(test))
 
-    t = 0
-    printProgressBar(t, 25200)
-    for [_, x] in train[:25200]:
+def kmeans(map_kohonen, W, sample):
+
+    # Asignación de centroides iniciales
+    means = []
+    for [label, x] in sample:
+        g = winner(W, x)
+        means.append([label, g])
+
+    # Actualización de centroides
+    iter = 0
+    printProgressBar(iter, 50)
+    while (iter < 15):
         minim = inf
-
-        for i in range(map_kohonen.shape[0]):
-            for j in range(map_kohonen.shape[1]):
-                map_kohonen[i, j] = d_euclidean(x, W[i, j, :])
-                if (map_kohonen[i, j] < minim):
-                    minim = map_kohonen[i, j]
-                    g = [i, j]
-        
         for i in range(W.shape[0]):
             for j in range(W.shape[1]):
-                W[i, j, :] = update_w(W[i, j, :], x, [i, j], g, t, lvq=1, ritmo_aprendizaje, vecindad)
-        
-        t += 1
-        printProgressBar(t, 25200)
-    
-    # Inicialización del mapa con algoritmo K-Means
-    sample = []
-    ctrl = list(range(0, 10))
-    for [label, x] in train[:25200]:
-        if (label in ctrl):
-            ctrl.remove(label)
-            sample.append(x)
-        if not ctrl:
-            break
-    
-    map_labels = np.zeros((100, 100)) # Mapa de labels
+                for [label, mean] in means:
+                    aux = d_euclidean(W[mean[0], mean[1], :], W[i, j, :])
+                    if (aux < minim):
+                        minim = aux
+                        map_kohonen[i, j] = label
 
-    for x in sample:
-        minim = inf
+        sum = np.zeros((10, W.shape[2]))
+        dim = np.zeros((10,))
         for i in range(map_kohonen.shape[0]):
             for j in range(map_kohonen.shape[1]):
-                aux = d_euclidean(x, W[i, j, :])
-                if (aux < minim):
-                    minim = aux
-                    g = [i, j]
+                label = int(map_kohonen[i, j])
+                sum[label, :] += W[i, j, :]
+                dim[label] += 1
+
+        means = []
+        for [label, mean] in means:
+            x = sum[label, :] / dim[label]
+            means.append(winner(W, x))
+
+        iter += 1
+        printProgressBar(iter, 50)
+    
+    return map_kohonen
+
+
+def printMap(map_kohonen):
+    """Plotear el mapa de kohonen marcando las diferentes clases
+
+    Parameters
+    ----------
+    map_kohonen : numpy array shape(i, j, k)
+        Mapa de kohonen a plotear
+    """
+    
+    map_data = {}
+    
+    for i in range(0, 10):
+        map_data[i] = []
+    
+    for i in range(map_kohonen.shape[0]):
+        for j in range(map_kohonen.shape[1]):
+            if (map_kohonen[i, j] < 0):
+                continue
+            index = int(map_kohonen[i, j])
+            map_data[index].append([i, j])
+    
+    for i in map_data:
+        for arr in map_data[i]:
+            plt.plot(arr[0], arr[1], label=str(i))
+    
+    return
+
+
+if __name__ == "__main__":
+
+    dataset = get_data('train.csv')
+
+    SUP = int(0.66 * len(dataset))
+    train_dataset = dataset[:SUP]
+    test_dataset = dataset[SUP:]
+
+    for trunc in range(1, 4):
+        LIMIT = int(trunc * len(train_dataset) / 4)
+        data = train_dataset[:LIMIT]
+
+        map_kohonen = np.zeros((20, 20)) # Mapa de neuronas
+        for i in range(map_kohonen.shape[0]):
+            for j in range(map_kohonen.shape[1]):
+                map_kohonen[i, j] = -1
+
+        W = np.zeros((map_kohonen.shape[0], map_kohonen.shape[1], data[0][1].shape[0]))
+
+        mu = 0
+        sigma = 1
+
+        for i in range(W.shape[0]):
+            for j in range(W.shape[1]):
+                W[i, j, :] = abs(0.01 * np.random.normal(mu, sigma, W.shape[2]))
+
+        MAX = int(0.66 * len(data))
+        train = data[:MAX]
+        test = data[MAX:]
+
+        print(len(data), len(train), len(test))
+
+        t = 0
+        printProgressBar(t, len(train))
+        for [_, x] in train:
+
+            g = winner(W, x)
+
+            for i in range(W.shape[0]):
+                for j in range(W.shape[1]):
+                    W[i, j, :] = update_w(W[i, j, :], x, [i, j], g, t, lvq=1)
+
+            t += 1
+            printProgressBar(t, len(train))
+
+
+        sample = []
+        ctrl = list(range(0, 10))
+        for [label, x] in train:
+            label = int(label)
+            if (label in ctrl):
+                ctrl.remove(label)
+                sample.append([label, x])
+            if not ctrl:
+                break
+
+
+        # def r_aux(a):
+        #     return 2
+
+        # for [label, x] in sample:
+        #     g = winner(W, x)
+        #     for i in range(W.shape[0]):
+        #         for j in range(W.shape[1]):
+        #             if (vecindad([i, j], g, 0, R=r_aux)):
+        #                 map_kohonen[i, j] = int(label)
+
+        map_kohonen = kmeans(map_kohonen, W, sample)
+
+
+        t = 0
+        printProgressBar(t, len(train))
+
+        for [label, x] in train:
+
+            label = int(label)
+            [i, j] = winner(W, x)
+
+            if (map_kohonen[i, j] == label):
+                lvq_c = 1
+            else:
+                lvq_c = -1
+
+            W[i, j, :] +=  W[i, j, :] + lvq_c * 0.01 * (x - W[i, j, :])
+
+            t += 1
+            printProgressBar(t, len(train))
+
+        print(map_kohonen)
+
+        t = 0
+        printProgressBar(t, len(test))
+
+        for [label, x] in test:
+
+            label = int(label)
+            [i, j] = winner(W, x)
+
+            if (map_kohonen[i, j] == label):
+                t +=1
+
+            printProgressBar(t, len(test))
+
+        print(t)
+
+        len(test)
+
+    t = 0
+    printProgressBar(t, len(test_dataset))
+
+    for [label, x] in test_dataset:
+
+        label = int(label)
+        [i, j] = winner(W, x)
+
+        if (map_kohonen[i, j] == label):
+            t +=1
+
+        printProgressBar(t, len(test_dataset))
+
+    print(t)
+
+    len(test_dataset)
+
